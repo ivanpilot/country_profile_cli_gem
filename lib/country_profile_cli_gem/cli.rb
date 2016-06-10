a7cb6acd957b2bcd7e07a316bc4fb574285bd260
Ivan Pilot <ivanpilot@gmail.com>
2016-06-11 00:11:08 +0100

    Improved CLI class by adding new  methods to take into account a user request to display indicators over a period of time

--- lib/country_profile_cli_gem/cli.rb
+++ lib/country_profile_cli_gem/cli.rb
@@ -27,23 +27,6 @@ class CountryProfileCliGem::CLI
     puts "\nSee you soon!"
   end
 
-  def sub_menu
-    input = nil
-
-    while input != "exit"
-      puts "\nif you would like to know about historical data for a specific indicator over a certain period of time, enter the number of the indicator, otherwise type \'exit\'"
-      input = gets.strip
-
-      if input == "exit"
-        input
-      elsif input.to_i.between?(1, 9)
-        puts "give me more details!!!!!!"
-      else
-        puts "\nI am not sure what you mean."
-      end
-    end
-  end
-
   def reformat_input(string)
     result = string.strip.split(" ").collect do |word|
       new_word = []
@@ -58,6 +41,35 @@ class CountryProfileCliGem::CLI
       new_word.join("")
     end
     result.join(" ")
+  end
+
+  def sub_menu
+    input = nil
+    @indicator_and_period = {}
+
+    while input != "exit"
+      puts "\nif you would like to know about historical data for a specific indicator over a certain period of time, enter the number of the indicator, otherwise type \'exit\'"
+      input = gets.strip
+
+      if input == "exit"
+        input
+      elsif input.to_i.between?(1, 9)
+        @indicator_and_period[:indicator_number] = input.to_i
+        @indicator_and_period[:indicator_period] = self.time_series
+        @indicator_and_period
+        # puts "#{@indicator_and_period}"
+      else
+        puts "\nI am not sure what you mean."
+      end
+    end
+    # @indicator_and_period = {}
+  end
+
+  def time_series
+    puts "\nPlease, provide the number of years you would like to go back in time. You can go back up to 40 years for certain countries."
+
+    input = gets.strip.to_i
+    input < 2 || input > 40 ? self.time_series : input
   end
 
   def list_countries
@@ -93,7 +105,7 @@ class CountryProfileCliGem::CLI
       puts ""
       puts "We are collecting the data for you........ bear with us........"
       puts ""
-      
+
       hash = self.country_card(country)
 
       puts "Country name: #{hash[:country_name]}"
