7c338137705ca174b7f44882158ca715b5540828
Ivan Pilot <ivanpilot@gmail.com>
2016-06-12 17:23:32 +0100

    indicator class should be done except for further modifications to potentially come after

--- lib/country_profile_cli_gem/constants.rb
+++ lib/country_profile_cli_gem/constants.rb
@@ -10,4 +10,15 @@ module CountryProfileCliGem
   LABOR_FORCE = "SL.TLF.TOTL.IN"
   UNEMPLOYMENT_RATE = "SL.UEM.TOTL.ZS"
 
+  # constants = {
+  #   :land_area => "AG.LND.TOTL.K2"
+  #   :total_population => "SP.POP.TOTL"
+  #   :female_population => "SP.POP.TOTL.FE.ZS"
+  #   :population_density => "EN.POP.DNST"
+  #   :gdp => "NY.GDP.MKTP.CD"
+  #   :gdp_growth => "NY.GDP.MKTP.KD.ZG"
+  #   :gdp_capita => "NY.GDP.PCAP.CD"
+  #   :labor_force => "SL.TLF.TOTL.IN"
+  #   :unemployment_rate => "SL.UEM.TOTL.ZS"
+  # }
 end
--- lib/country_profile_cli_gem/indicator.rb
+++ lib/country_profile_cli_gem/indicator.rb
@@ -1,12 +1,16 @@
+require_relative "../country_profile_cli_gem"
+# require_relative "./version"
+# require 'pry'
+
 class CountryProfileCliGem::Indicator
 
   YEAR_END = 2014
 
-  attr_accessor :name, :country, :time_period, :year_begin
+  attr_accessor :country, :name, :time_period, :year_begin
 
-  def initialize(name, country, time_period = 1)
-    @name = name
+  def initialize(country, name = nil, time_period = 1)
     @country = country
+    @name = name
     @time_period = time_period
     @year_begin = YEAR_END - @time_period + 1
   end
@@ -15,37 +19,40 @@ class CountryProfileCliGem::Indicator
     uri = URI.parse("http://api.worldbank.org/countries/" + self.country + "?format=json")
     response = Net::HTTP.get_response(uri)
     json = JSON.parse(response.body)
-
-    {
-      country_name: json[1][0]["name"],
-      country_isocode: json[1][0]["id"],
-      capital_city: json[1][0]["capitalCity"],
-      longitude: json[1][0]["longitude"],
-      latitude: json[1][0]["latitude"],
-    }
+    if json.count == 1
+      begin
+        raise CountryProfileCliGem::CountryError
+      rescue CountryProfileCliGem::CountryError => error
+        puts error.message
+      end
+    else
+      {
+        country_name: json[1][0]["name"],
+        country_isocode: json[1][0]["id"],
+        capital_city: json[1][0]["capitalCity"],
+        longitude: json[1][0]["longitude"],
+        latitude: json[1][0]["latitude"],
+      }
+    end
   end
 
   def macro_indicators
     uri = URI.parse("http://api.worldbank.org/countries/" + self.country + "/indicators/" + self.name + "?date=" + self.year_begin.to_s + ":" + YEAR_END.to_s + "&format=json")
     response = Net::HTTP.get_response(uri)
     json = JSON.parse(response.body)
-    array = json[1]
 
-    # indicator_time_series = {}
-    # end_year = YEAR_END.to_i
-    array.collect do |year|
-      # indicator_time_series["counter".to_sym] =
-      array[year]["value"]
-      # end_year -= 1
+    indicator_time_series = {}
+    json[1].each_with_index do |element, index|
+      indicator_time_series[self.name.instance_variables + "_" + (YEAR_END - index).to_s] = element["value"]
     end
-    # indicator_time_series
+    indicator_time_series
   end
-  # {
-  #   self.name.to_sym => json[1][0]["value"]
-  # }
-
-
 
 end
 
-# brazil = br.new("NY.GDP.MKTP.CD", "bra", 10)
+# br = CountryProfileCliGem::Indicator.new("bra", CountryProfileCliGem::LAND_AREA, 10)
+# br.macro_indicators
+# br = CountryProfileCliGem::Indicator.new("bra", CountryProfileCliGem::LAND_AREA, 10)
+# br.standard_indicators
+br = CountryProfileCliGem::Indicator.new("bra")
+br.standard_indicators
