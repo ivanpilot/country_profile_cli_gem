d0559093c6edd5222f9691c61988e867415fcff1
Ivan Pilot <ivanpilot@gmail.com>
2016-06-14 21:45:14 +0100

    output class working properly and outputting data with appropriate format

--- lib/country_profile_cli_gem/constants.rb
+++ lib/country_profile_cli_gem/constants.rb
@@ -10,15 +10,27 @@ module CountryProfileCliGem
   LABOR_FORCE = "SL.TLF.TOTL.IN"
   UNEMPLOYMENT_RATE = "SL.UEM.TOTL.ZS"
 
-  # constants = {
-  #   :land_area => "AG.LND.TOTL.K2"
-  #   :total_population => "SP.POP.TOTL"
-  #   :female_population => "SP.POP.TOTL.FE.ZS"
-  #   :population_density => "EN.POP.DNST"
-  #   :gdp => "NY.GDP.MKTP.CD"
-  #   :gdp_growth => "NY.GDP.MKTP.KD.ZG"
-  #   :gdp_capita => "NY.GDP.PCAP.CD"
-  #   :labor_force => "SL.TLF.TOTL.IN"
-  #   :unemployment_rate => "SL.UEM.TOTL.ZS"
-  # }
+  CONSTANTS = {
+    :land_area => "AG.LND.TOTL.K2",
+    :total_population => "SP.POP.TOTL",
+    :female_population => "SP.POP.TOTL.FE.ZS",
+    :male_population => "SP.POP.TOTL.FE.ZS",
+    :population_density => "EN.POP.DNST",
+    :gdp => "NY.GDP.MKTP.CD",
+    :gdp_growth => "NY.GDP.MKTP.KD.ZG",
+    :gdp_capita => "NY.GDP.PCAP.CD",
+    :labor_force => "SL.TLF.TOTL.IN",
+    :unemployment_rate => "SL.UEM.TOTL.ZS"
+  }
+
+  UNITS = {
+    "SP.POP.TOTL" => "m ppl",
+    "SP.POP.TOTL.FE.ZS" => "%",
+    "EN.POP.DNST" => "ppl/sq. km",
+    "NY.GDP.MKTP.CD" => "current $US bn",
+    "NY.GDP.MKTP.KD.ZG" => "annual %",
+    "NY.GDP.PCAP.CD" => "current $US",
+    "SL.TLF.TOTL.IN" => "m ppl",
+    "SL.UEM.TOTL.ZS" => "% of total labor force"
+  }
 end
--- lib/country_profile_cli_gem/output.rb
+++ lib/country_profile_cli_gem/output.rb
@@ -37,15 +37,15 @@ class CountryProfileCliGem::Output
     puts ""
 
     standard_indicators = self.output_standard_indicators
-    land_area = self.output_macro_indicators(CountryProfileCliGem::LAND_AREA)
-    total_population = self.output_macro_indicators(CountryProfileCliGem::TOTAL_POPULATION)
-    female_population = self.output_macro_indicators(CountryProfileCliGem::FEMALE_POPULATION)
-    population_density = self.output_macro_indicators(CountryProfileCliGem::POPULATION_DENSITY)
-    gdp = self.output_macro_indicators(CountryProfileCliGem::GDP)
-    gdp_growth = self.output_macro_indicators(CountryProfileCliGem::GDP_GROWTH)
-    gdp_capita = self.output_macro_indicators(CountryProfileCliGem::GDP_CAPITA)
-    labor_force = self.output_macro_indicators(CountryProfileCliGem::LABOR_FORCE)
-    unemployment_rate = self.output_macro_indicators(CountryProfileCliGem::UNEMPLOYMENT_RATE)
+    land_area = self.output_macro_indicators(CountryProfileCliGem::CONSTANTS[:land_area])
+    total_population = self.output_macro_indicators(CountryProfileCliGem::CONSTANTS[:total_population])
+    female_population = self.output_macro_indicators(CountryProfileCliGem::CONSTANTS[:female_population])
+    population_density = self.output_macro_indicators(CountryProfileCliGem::CONSTANTS[:population_density])
+    gdp = self.output_macro_indicators(CountryProfileCliGem::CONSTANTS[:gdp])
+    gdp_growth = self.output_macro_indicators(CountryProfileCliGem::CONSTANTS[:gdp_growth])
+    gdp_capita = self.output_macro_indicators(CountryProfileCliGem::CONSTANTS[:gdp_capita])
+    labor_force = self.output_macro_indicators(CountryProfileCliGem::CONSTANTS[:labor_force])
+    unemployment_rate = self.output_macro_indicators(CountryProfileCliGem::CONSTANTS[:unemployment_rate])
 
     puts "Country name: #{standard_indicators[:country_name]}"
     puts "Country isocode: #{standard_indicators[:country_isocode]}"
@@ -73,13 +73,60 @@ class CountryProfileCliGem::Output
     puts "All data and information presented above are sourced from the World Bank database and are displayed for the year 2014."
   end
 
-  def display_indicator_time_series(indicator_name, time_period = 1)
-    indicator = self.output_macro_indicators(indicator_name, time_period)
+  def display_indicator_time_series(constants_key, time_period = 1)
+    if constants_key == "male_population"
+      display_male_indicator_time_series(time_period)
+    else
+      display_general_indicator_time_series(CountryProfileCliGem::CONSTANTS[constants_key.to_sym], time_period)
+    end
+  end
+
+  def display_male_indicator_time_series(time_period = 1)
+    puts ""
+    puts "We are collecting the data for you........ bear with us........"
+    puts ""
+
+    indicator = self.output_macro_indicators("SP.POP.TOTL.FE.ZS", time_period)
+
+    puts "The evolution of the male population for #{self.country.name} over the last #{time_period} year(s): (data units are in %)"
 
     indicator.each do |year, value|
-      puts "#{year.to_s}: #{value}"
+      puts "  #{year.to_s}: #{100 - value.to_f.round(1)}"
     end
+  end
 
+  def display_general_indicator_time_series(name, time_period = 1)
+    puts ""
+    puts "We are collecting the data for you........ bear with us........"
+    puts ""
+
+    indicator_name = CountryProfileCliGem::CONSTANTS.key(name).to_s
+    unit = CountryProfileCliGem::UNITS[name]
+
+    indicator = self.output_macro_indicators(name, time_period)
+
+    puts "The evolution of the #{indicator_name} for #{self.country.name} over the last #{time_period} year(s): (data units are in #{unit})"
+
+    amount = nil
+    indicator.each do |year, value|
+      amount = case unit
+      when "m ppl"
+        value.to_i / 1000000
+      when "%"
+        value.to_f.round(1)
+      when "annual %"
+        value.to_f.round(1)
+      when "% of total labor force"
+        value.to_f.round(1)
+      when "ppl/sq. km"
+        value.to_i
+      when "current $US bn"
+        value.to_i / 1000000000
+      when "current $US"
+        value.to_i
+      end
+      puts "  #{year.to_s}: #{amount}"
+    end
   end
 
   def self.all
